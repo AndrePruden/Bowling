71057dc766013a3a18ccf1e51498db4961ac6e8f
AndrePruden <andrepruden0@gmail.com>
2024-03-17 15:05:52 -0400

    Tercer commit con modificaicones en el codigo

--- src/bowling.test.js
+++ src/bowling.test.js
@@ -1,18 +1,34 @@
 //import sumar from "./sumador.js";
+class Game{
+  constructor()
+  {
+     this.puntaje = 0;
+  }
+
+  roll(n)
+  {
+    this.puntaje += n;
+  }
+
+  score()
+  {
+    return this.puntaje;
+  }
+}
+
+let BowlingGame = new Game();
 
 describe("Score Bowling", () => {
   it("Deberia retornar 0", () => {
-    let BowlingGame = new Game();
-    for(var i=0; i<20; i++){
-        BowlingGame.roll(0);
-    }
+    var n = 20;
+    var pins = 0;
+    rollMany(n,pins);
     expect(BowlingGame.score()).toEqual(0);
   });
 });
 
 describe("Score Bowling", () => {
   it("Deberia retornar 20", () => {
-    let BowlingGame = new Game();
     for(var i=0; i<20; i++){
         BowlingGame.roll(1);
     }
@@ -20,19 +36,7 @@ describe("Score Bowling", () => {
   });
 });
 
-class Game{
-    constructor()
-    {
-       this.puntaje = 0;
-    }
-
-    roll(n)
-    {
-      this.puntaje += n;
-    }
-
-    score()
-    {
-      return this.puntaje;
-    }
+function rollMany(n, pins){
+  for (var i = 0; i < n; i++)
+    BowlingGame.roll(pins);
 }
