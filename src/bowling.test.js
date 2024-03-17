384b0f12bfb9cf8eda0b50d6f0e62e4451927a22
AndrePruden <andrepruden0@gmail.com>
2024-03-17 16:01:48 -0400

    Quinto commit con la regla del Strike

--- src/bowling.test.js
+++ src/bowling.test.js
@@ -15,21 +15,46 @@ class Game{
   score()
   {
     this.puntaje = 0;
-    this.i = 0;
+    this.frameIndex = 0;
     for (var frame = 0; frame < 10; frame++){
-      if(this.rolls[this.i] + this.rolls[this.i+1] == 10) // Spare
+      if(this.rolls[this.frameIndex] == 10) // Strike
       {
-        this.puntaje += 10 + this.rolls[this.i+2];
-        this.i += 2;
+        this.puntaje += 10 + this.strikeBonus(this.frameIndex);
+        this.frameIndex++;
+      }
+      else if(this.isSpare(this.frameIndex)) // Spare
+      {
+        this.puntaje += 10 + this.spareBonus(this.frameIndex);
+        this.frameIndex += 2;
       }
       else
       {
-        this.puntaje += this.rolls[this.i] + this.rolls[this.i+1];
-        this.i += 2;
+        this.puntaje += this.sumOfBallsInFrame(this.frameIndex);
+        this.frameIndex += 2;
       }
     }
 
     return this.puntaje;
+  }
+
+  sumOfBallsInFrame(frameIndex)
+  {
+    return this.rolls[frameIndex] + this.rolls[frameIndex+1];
+  }
+
+  isSpare(frameIndex)
+  {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10;
+  }
+
+  spareBonus(frameIndex)
+  {
+    return this.rolls[frameIndex+2];
+  }
+
+  strikeBonus(frameIndex)
+  {
+    return this.rolls[frameIndex+1] + this.rolls[frameIndex+2];
   }
 }
 
@@ -53,15 +78,34 @@ describe("Score Bowling", () => {
 describe("Score Bowling", () => {
   it("Spare", () => {
     let BowlingGame = new Game();
-    BowlingGame.roll(5);
-    BowlingGame.roll(5); //Spare
+    rollSpare(BowlingGame);
     BowlingGame.roll(3);
     rollMany(BowlingGame,17,0);
     expect(BowlingGame.score()).toEqual(16);
   });
 });
 
+describe("Score Bowling", () => {
+  it("Strike", () => {
+    let BowlingGame = new Game();
+    rollStrike(BowlingGame);
+    BowlingGame.roll(3);
+    BowlingGame.roll(4);
+    rollMany(BowlingGame, 16, 0);
+    expect(BowlingGame.score()).toEqual(24);
+  });
+});
+
 function rollMany(BowlingGame, n, pins){
   for (var i = 0; i < n; i++)
     BowlingGame.roll(pins);
+}
+
+function rollSpare(BowlingGame){
+  BowlingGame.roll(5);
+  BowlingGame.roll(5);
+}
+
+function rollStrike(BowlingGame){
+  BowlingGame.roll(10);
 }
